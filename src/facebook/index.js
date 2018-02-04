5bb17644c1a73e7993613933a66ac3de3471fb33
Terry Schmidt <32104933+terry-schmidt@users.noreply.github.com>
2018-02-04 16:33:17 -0700

    Another revision to the regular expression

--- src/facebook/index.js
+++ src/facebook/index.js
@@ -1,51 +1,46 @@
 module.exports = function (context, req) {
-  if (req.method === "GET") {
-    // handle new subscription request
-    let mode = req.query["hub.mode"];
-    let challenge = req.query["hub.challenge"];
-    let verify = req.query["hub.verify_token"];
+	if (req.method === "GET") {
+		// handle new subscription request
+		let mode = req.query["hub.mode"];
+		let challenge = req.query["hub.challenge"];
+		let verify = req.query["hub.verify_token"];
 
-    if (verify === process.env.FB_VERIFY_TOKEN) {
-      // if verification matches, return challenge
-      context.res.raw(challenge);
-    } else {
-      context.res.sendStatus(400);
-    }
-  }
-  else if (req.method === "POST") {
-	  context.log('HTTP Post triggered...');
-	  context.log('Expression:' + "/(#" + process.env.HASHTAG + ")($|[\s\n.,]+)/");
-	let entries = req.body.entry;
-	// Old expression that was converted to be more dynamic:  /(#hfm)($|[\s\n.,]+)/
-    let hfmRx = new RegExp("(#" + process.env.HASHTAG + ")($|[\\s\\n.,]+)", "igm");
+		if (verify === process.env.FB_VERIFY_TOKEN) {
+			// if verification matches, return challenge
+			context.res.raw(challenge);
+		} else {
+			context.res.sendStatus(400);
+		}
+	}
+	else if (req.method === "POST") {
+		context.log('HTTP Post triggered...');
+		let entries = req.body.entry;
+		// Old expression that was converted to be more dynamic:  /(#hfm)($|[\s\n.,]+)/
+		// Also remove the space / newline checks, as even if it is #HFMo, it should probably trigger for safety sake
+		//let hfmRx = new RegExp("(#" + process.env.HASHTAG + ")($|[\\s\\n.,]+)", "igm");
+		let hfmRx = new RegExp("(#" + process.env.HASHTAG + ")", "igm");
 
-    if (entries) {
-      entries.forEach(function (entry) {
-        entry.changes.forEach(function (change) {
-			context.log('Change Field: ' + change.field);
-			context.log('Change Value: ' + change.value);
-          if (change.field === "status") {
-            if (change.value.match(hfmRx)) {
-				context.log('HFM Hashtag match found...');
-              var data = {
-                platform: 'facebook',
-                userid: entry.id,
-                mediaid: change.id,
-                facebook: {
-                  post: change.value
-                }
-              };
+		if (entries) {
+			entries.forEach(function (entry) {
+				entry.changes.forEach(function (change) {
+					if (change.field === "status") {
+						if (change.value.match(hfmRx)) {
+							context.log('HFM Hashtag match found in: ' + change.value);
+							var data = {
+								platform: 'facebook',
+								userid: entry.id,
+								mediaid: change.id,
+								facebook: {
+									post: change.value
+								}
+							};
 
-              context.bindings.out = data;
-            }
-          }
-        });
-      });
-    }
-    context.res.sendStatus(200);
-  };
+							context.bindings.out = data;
+						}
+					}
+				});
+			});
+		}
+		context.res.sendStatus(200);
+	};
 };
-
-function escapeRegex(value) {
-    return value.replace( /[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&" );
-}
