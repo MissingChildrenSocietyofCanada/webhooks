82adb1cec49c918d37451751da7ca250d464e18a
Terry Schmidt <32104933+terry-schmidt@users.noreply.github.com>
2018-02-05 21:43:10 -0700

    Updated webhooks with more consistent logging

--- src/facebook/index.js
+++ src/facebook/index.js
@@ -1,19 +1,30 @@
+// Webhook:  Facebook
+// ------------------
+// The GET portion handles the registration of the webhook subscription, returning back to Facebook the challenge sent.
+// The POST portion is triggered whenever the a registered Facebook user updates a field tracked in the subscription.
+// If the status field is updated, a queue message is created containing the post.
+
 module.exports = function (context, req) {
 	if (req.method === "GET") {
+		context.log('Verifying subscription registration...');
+
 		// handle new subscription request
 		let mode = req.query["hub.mode"];
 		let challenge = req.query["hub.challenge"];
 		let verify = req.query["hub.verify_token"];
 
 		if (verify === process.env.FB_VERIFY_TOKEN) {
+			context.log('Returning challenge...');
 			// if verification matches, return challenge
 			context.res.raw(challenge);
 		} else {
+			context.log.warn('Facebook token not verified.');
 			context.res.sendStatus(400);
 		}
 	}
 	else if (req.method === "POST") {
-		context.log('HTTP Post triggered...');
+		context.log('Subscription triggered...');
+
 		let entries = req.body.entry;
 		// Old expression that was converted to be more dynamic:  /(#hfm)($|[\s\n.,]+)/
 		// Also remove the space / newline checks, as even if it is #HFMo, it should probably trigger for safety sake
@@ -21,14 +32,18 @@ module.exports = function (context, req) {
 		let hfmRx = new RegExp("(#" + process.env.HASHTAG + ")", "igm");
 
 		if (entries) {
-			context.log('Has entries...');
+			context.log('Request has entries...');
+
 			entries.forEach(function (entry) {
 				context.log('Looping through changes in each entry...');
+
 				entry.changes.forEach(function (change) {
 					context.log('Checking change field: ' + change.field);
+
 					if (change.field === "status") {
 						if (change.value.match(hfmRx)) {
 							context.log('HFM Hashtag match found in: ' + change.value);
+
 							var data = {
 								platform: 'facebook',
 								userid: entry.id,
@@ -38,12 +53,14 @@ module.exports = function (context, req) {
 								}
 							};
 
+							context.log({'Data sent to queue': data});
 							context.bindings.out = data;
 						}
 					}
 				});
 			});
 		}
+
 		context.res.sendStatus(200);
 	};
 };
